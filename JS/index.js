c7c6b9ea607c18f8c2a70bec378e5e3b41c14283
melashu <meshu102@outlook.com>
2023-11-16 00:05:41 +0300

    Fetch json data using fetch API

--- JS/index.js
+++ JS/index.js
@@ -1,23 +1,30 @@
-import data from '../data.json' assert { type: 'json' };
 import countries from '../country-list.js';
-
-const parsedData = JSON.parse(data);
 const country = document.getElementById('country');
 const countryList = document.getElementById('country-list');
 const priceContainer = document.getElementById('price-container');
 const currency = document.getElementById('currency');
+const priceP = document.createElement("p");
 
-const priceP = document.createElement("p")
+const getPrice = async () => {
+    let response = await fetch("../data.json");
+    let data = await response.json();
+    return data;
+}
 
-country.addEventListener("change", (e) => {
-    const value = e.target.value
-    const price = parsedData[value]["price"];
-    const currencyValue = country.dataset.currency
+getPrice().then(data => {
+    const parsedData = JSON.parse(data);
+    country.addEventListener("change", (e) => {
+        const value = e.target.value
+        const price = parsedData[value]["price"];
+        const currencyValue = country.dataset.currency
 
-    priceContainer.innerHTML = `<h2 class='title'>Avaliable SMS Price in ${countries[value]}</h2>`
-    priceP.innerText = `${currencyValue}: ${price[currencyValue]}`;
-    priceContainer.appendChild(priceP)
+        priceContainer.innerHTML = `<h2 class='title'>Avaliable SMS Price in ${countries[value]}</h2>`
+        priceP.innerText = `${currencyValue}: ${price[currencyValue]}`;
+        priceContainer.appendChild(priceP)
+    })
 })
+
+
 
 Object.keys(countries).forEach(key => {
     const option = document.createElement('option');
@@ -25,3 +32,4 @@ Object.keys(countries).forEach(key => {
     option.innerText = countries[key];
     countryList.appendChild(option);
 });
+
